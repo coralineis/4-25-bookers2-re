96c0fa5929b0805a31d9ebf55572647d95953fa8
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-13 13:56:18 +0000

    Add homes,books,usersコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
