7781a695a5d0c56a54348ace4d70fe3f8891047a
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-14 02:49:25 +0000

    Add Booksコントローラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+        flash[:success] = "You have created book successfully."
+        redirect_to book_path(@book.id)
+    else
+        @books = Book.all
+        render :index
+    end
   end
 
   def index
     @book = Book.new
-    @book.user_id = current_user.id
+    @user = User.find(params[:id])
     @books = Book.all
   end
 
@@ -19,6 +24,26 @@ class BooksController < ApplicationController
   end
 
   def edit
+     @book = Book.find(params[id])
+    unless @book.user != current_user
+      redirect_to book_path(@book.id)
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:success] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
